f02896390a6a8774d80101f2d75df5308f1d0749
Oleksii Shtanko <Oleksii_Shtanko1@epam.com>
2022-10-12 23:03:22 +0200

    implement task 2

--- Task1_Core/Program.cs
+++ Task1_Core/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
+using System.Linq;
 using Task1_Lib;
 
 namespace Task1_Core
 {
     class Program
     {
+        public class Product
+        {
+            public Product(string name, double price)
+            {
+                Name = name;
+                Price = price;
+            }
+
+            public string Name { get; set; }
+
+            public double Price { get; set; }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter username:");
-            string userName = Console.ReadLine();
-            Console.WriteLine(Lib.OutPutLogic(userName));
+            var products = new Product[]
+            {
+                new Product("Product 1", 10.0d),
+                new Product("Product 2", 20.0d),
+                new Product("Product 3", 30.0d),
+            };
+            var productToFind = products[2];
+            Console.WriteLine(IndexOf(products, x=>x.Equals(productToFind)));
+        }
+        public static int IndexOf(Product[] products, Predicate<Product> predicate)
+        {
+            if (predicate is null || products is null)
+                throw new ArgumentNullException();
+
+            for (int i = 0; i < products.Length - 1; i++)
+            {
+                var product = products[i];
+                if (predicate(product))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
     }
 }
