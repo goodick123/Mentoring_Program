247a69615e669af4a075c3342e38849091a8908d
Oleksii Shtanko <Oleksii_Shtanko1@epam.com>
2022-11-12 15:44:03 +0100

    implement task4

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ConsoleKey input = ConsoleKey.Enter;
+            while (input != ConsoleKey.Escape)
+            {
+                string userInput = Console.ReadLine();
+                try
+                {
+                    char firstLetter = userInput[0];
+                    Console.WriteLine(firstLetter);
+                }
+                catch
+                {
+                    Console.WriteLine("Empty string is to allowed");
+                }
+                Console.WriteLine("press Escape if you want to finish");
+                input = Console.ReadKey().Key;
+            }
+        }
+    }
+}
