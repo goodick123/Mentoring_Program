577da1eccf3b09d19e4dc9dd250742cfe931cfd1
Oleksii Shtanko <Oleksii_Shtanko1@epam.com>
2022-11-15 17:07:35 +0100

    fix comments

--- Task2/NumberParser.cs
+++ Task2/NumberParser.cs
@@ -8,61 +8,67 @@ namespace Task2
     {
         public int Parse(string stringValue)
         {
+            ValidateInput(stringValue);
+            int intResult = 0;
+            switch (stringValue[0])
+            {
+                case '-':
+                    {
+                        foreach (var stringChar in stringValue.Remove(0, 1))
+                        {
+                            intResult = ConvertStringToInt(stringChar, intResult);
+                        }
+
+                        intResult *= -1;
+                        break;
+                    }
+                case '+':
+                    {
+                        foreach (var stringChar in stringValue.Remove(0, 1))
+                        {
+                            intResult = ConvertStringToInt(stringChar, intResult);
+                        }
+
+                        break;
+                    }
+                default:
+                    foreach (var stringChar in stringValue)
+                    {
+                        intResult = ConvertStringToInt(stringChar, intResult);
+                    }
+
+                    break;
+            }
+            return intResult;
+        }
+
+        public void ValidateInput(string stringValue)
+        {
             if (stringValue == null)
                 throw new ArgumentNullException();
-            if (stringValue=="" || char.IsWhiteSpace(stringValue[0]))
+            if (stringValue == "" || char.IsWhiteSpace(stringValue[0]))
                 throw new FormatException();
 
-            char[] numbers = { '-',' ','+', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            char[] numbers = { '-', ' ', '+', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
             for (int i = 0; i < stringValue.Length; i++)
             {
-                if (!numbers.Contains(stringValue[i]) || (stringValue[i]=='-' && i!=0) || (stringValue[i] == '+' && i != 0))
+                if (!numbers.Contains(stringValue[i]) || (stringValue[i] == '-' && i != 0) || (stringValue[i] == '+' && i != 0))
                     throw new FormatException();
             }
 
-            if ((stringValue.Length == 10 && stringValue[0]!= '-' && !Regex.IsMatch(stringValue, "^([1 - 2][0 - 1][0 - 4][0 - 7][0 - 4][0 - 8][0 - 3][0 - 6][0 - 4][0 - 7])$")) || (stringValue.Length == 11 && !Regex.IsMatch(stringValue, "^(-[1 - 2][0 - 1][0 - 4][0 - 7][0 - 4][0 - 8][0 - 3][0 - 6][0 - 4][0 - 8])$")) || stringValue.Length > 11)
+            if ((stringValue.Length == 10 && stringValue[0] != '-' && !Regex.IsMatch(stringValue, "^([1 - 2][0 - 1][0 - 4][0 - 7][0 - 4][0 - 8][0 - 3][0 - 6][0 - 4][0 - 7])$")) || (stringValue.Length == 11 && !Regex.IsMatch(stringValue, "^(-[1 - 2][0 - 1][0 - 4][0 - 7][0 - 4][0 - 8][0 - 3][0 - 6][0 - 4][0 - 8])$")) || stringValue.Length > 11)
             {
                 throw new OverflowException();
             }
+        }
 
-            int num = 0;
-            int n = stringValue.Length;
-            var counter = 0;
-            if (stringValue[0] == '-')
+        public int ConvertStringToInt(char inputChar, int result)
+        {
+            if (!(inputChar == 0) && !char.IsWhiteSpace(inputChar))
             {
-                for (int i = 1; i < n; i++)
-                    if ((stringValue[i] == 0 && counter == 0) || char.IsWhiteSpace(stringValue[i]))
-                        continue;
-                    else
-                    {
-                        num = num * 10 + (stringValue[i] - 48);
-                        counter++;
-                    }
-                num *= -1;
+                result = result * 10 + (inputChar - 48);
             }
-            else if (stringValue[0] == '+')
-            {
-                for (int i = 1; i < n; i++)
-                    if ((stringValue[i] == 0 && counter == 0) || char.IsWhiteSpace(stringValue[i]))
-                        continue;
-                    else
-                    {
-                        num = num * 10 + (stringValue[i] - 48);
-                        counter++;
-                    }
-            }
-            else
-            {
-                for (int i = 0; i < n; i++)
-                    if ((stringValue[i] == 0 && counter == 0) || char.IsWhiteSpace(stringValue[i]))
-                        continue;
-                    else
-                    {
-                        num = num * 10 + (stringValue[i] - 48);
-                        counter++;
-                    }
-            }
-            return num;
+            return result;
         }
     }
 }
