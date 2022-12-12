914d2ce219d5a28ba5fd6d60c0422809ee3b8631
Oleksii Shtanko <Oleksii_Shtanko1@epam.com>
2022-12-12 15:43:26 +0100

    add circularPrimes class

---
+++ UnitTesting/CircularPrimes.cs
@@ -0,0 +1,58 @@
+﻿
+namespace UnitTesting
+{
+    public class CircularPrimes
+    {
+        public int GetCircularPrimes(int num)
+        {
+            var count = 0;
+
+                for (int i = 0; i <= num; i++)
+                {
+                    if (CheckCircular(i))
+                        count++;
+                }
+
+                return count;
+        }
+
+        private bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+            if (n <= 3)
+                return true;
+
+            if (n % 2 == 0 || n % 3 == 0)
+                return false;
+
+            for (int i = 5; i * i <= n; i = i + 6)
+                if (n % i == 0 || n % (i + 2) == 0)
+                    return false;
+
+            return true;
+        }
+        
+        private bool CheckCircular(int N)
+        {
+            int count = 0, temp = N;
+            while (temp > 0)
+            {
+                count++;
+                temp /= 10;
+            }
+
+            int num = N;
+            while (IsPrime(num))
+            {
+                int rem = num % 10;
+                int div = num / 10;
+                num = (int)(Math.Pow(10, count -1) * rem) + div;
+                if (num == N)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ UnitTesting/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace UnitTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select value from list");
+            Console.WriteLine("1. Circular primes");
+            Console.WriteLine("Enter number you want to select");
+
+            var numberKata = Console.ReadLine();
+
+            if (numberKata == "1")
+            {
+                Console.WriteLine("Circular primes kata");
+                Console.WriteLine("Enter number");
+
+                var num = Console.ReadLine();
+                int parseNum;
+
+                if (int.TryParse(num, out parseNum))
+                {
+                    CircularPrimes primes = new();
+                    Console.WriteLine("Sum of all prime numbers: " + primes.GetCircularPrimes(parseNum));
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid number");
+
+            }
+        }
+    }
+}
